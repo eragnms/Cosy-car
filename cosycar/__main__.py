38a9ff4cf27e9f236190e304cfe01d5ca8425864
Mats Gustafsson <hem@mats-gustafsson.se>
2017-10-01 17:47:31 +0200

    upd structure

--- cosycar/__main__.py
+++ cosycar/__main__.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # see https://softwareengineering.stackexchange.com/questions/333761/where-do-you-put-the-main-function-of-a-python-app
+import pkg_resources
 
-# Version numbering scheme, see
-# https://packaging.python.org/distributing/#choosing-a-versioning-scheme
-# 1.2.0.dev1  # Development release
-# 1.2.0a1     # Alpha Release
-# 1.2.0b1     # Beta Release
-# 1.2.0rc1    # Release Candidate
-# 1.2.0       # Final Release
-# 1.2.0.post1 # Post Release
-__version__ = '4.0.0.dev26'
 
 def main():
     print('Hello world!')
-
+    version = pkg_resources.get_distribution("cosycar").version
+    print(version)
 
 if __name__ == "__main__":
     main()
