6e49036fa8bee03513f3a5d7b5b8b2e2b057457c
Mats Gustafsson <hem@mats-gustafsson.se>
2017-10-15 16:30:43 +0200

    adds code to integration tests

--- tests/cosycar_integration.py
+++ tests/cosycar_integration.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
+import time
 
+PORT = 8085
+HTTP_LOG_FILE = 'tests/data/http_log_file.log'
+NO_HEATER_RUNNING = 0
+BLOCK_HEATER_RUNNING = 1
+COMP_HEATER_1_RUNNING = 2
+COMP_HEATER_2_RUNNING = 4
+INTERVALS = [(10, NO_HEATER_RUNNING),
+             (10, BLOCK_HEATER_RUNNING),
+             (10, BLOCK_HEATER_RUNNING | COMP_HEATER_1_RUNNING),
+             (10, NO_HEATER_RUNNING)]
 
 class IntegrationError(Exception):
     def __init__(self, value):
@@ -10,16 +21,32 @@ class IntegrationError(Exception):
         return repr(self.value)
 
 def setup():
-    pass
+    os.remove(HTTP_LOG_FILE)
+    # Modify config file
+    # Start HTTP monitor
 
 def teardown():
     pass
 
 setup()
+
+
+total_run_time = sum of intervals...
+start_time = time.time()
+end_time = start_time + 
+now = time.time()
+
+while now < end_time:
+    
+    now = time.time()
+
+
+
 result = os.system('cosycar >/dev/null 2>&1')
 if result:
     teardown()
     raise IntegrationError('Integration test failed with: {}'.format(result))
 else:
     print('Integration test OK!')
+
 teardown()
