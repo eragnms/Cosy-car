d19edb69f107193cdb659410051b0e80e63dbb93
Mats Gustafsson <hem@mats-gustafsson.se>
2017-10-15 08:14:52 +0200

    changes reflect to write to a log file instead of screen

--- tests/reflect.py
+++ tests/reflect.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
+
 # Reflects the requests from HTTP methods GET, POST, PUT, and DELETE
-# Written by Nathan Hamiel (2010)
+# Orinally written by Nathan Hamiel (2010), see:
+# https://gist.github.com/1kastner/e083f9e813c0464e6a2ec8910553e632
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from optparse import OptionParser
+
+PORT = 8085
+HTTP_LOG_FILE = 'tests/data/http_log_file.log'
 
 class RequestHandler(BaseHTTPRequestHandler):
     
     def do_GET(self):
         
         request_path = self.path
-        
-        print("\n----- Request Start ----->\n")
-        print("Request path:", request_path)
-        print("Request headers:", self.headers)
-        print("<----- Request End -----\n")
+        with open(HTTP_LOG_FILE, 'a') as log_file:
+            log_file.write("\n----- GET Request Start ----->\n")
+            log_file.write("Request path:", request_path)
+            log_file.write("Request headers:", self.headers)
+            log_file.write("<----- Request End -----\n")
         
         self.send_response(200)
         self.send_header("Set-Cookie", "foo=bar")
@@ -23,18 +28,16 @@ class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         
         request_path = self.path
-        
-        print("\n----- Request Start ----->\n")
-        print("Request path:", request_path)
-        
-        request_headers = self.headers
-        content_length = request_headers.get('Content-Length')
-        length = int(content_length) if content_length else 0
-        
-        print("Content Length:", length)
-        print("Request headers:", request_headers)
-        print("Request payload:", self.rfile.read(length))
-        print("<----- Request End -----\n")
+        with open(HTTP_LOG_FILE, 'a') as log_file:
+            log_file.write("\n----- POST Request Start ----->\n")
+            log_file.write("Request path:", request_path)
+            request_headers = self.headers
+            content_length = request_headers.get('Content-Length')
+            length = int(content_length) if content_length else 0
+            log_file.write("Content Length:", length)
+            log_file.write("Request headers:", request_headers)
+            log_file.write("Request payload:", self.rfile.read(length))
+            log_file.write("<----- Request End -----\n")
         
         self.send_response(200)
         self.end_headers()
@@ -43,18 +46,16 @@ class RequestHandler(BaseHTTPRequestHandler):
     do_DELETE = do_GET
         
 def main():
-    port = 8085
-    print('Listening on localhost:%s' % port)
+    port = PORT
+    #print('Listening on localhost:%s' % port)
     server = HTTPServer(('', port), RequestHandler)
     server.serve_forever()
 
         
 if __name__ == "__main__":
-    print('hej')
     parser = OptionParser()
     parser.usage = ("Creates an http-server that will echo out any GET or POST parameters\n"
                     "Run:\n\n"
                     "   reflect")
     (options, args) = parser.parse_args()
-    
     main()
