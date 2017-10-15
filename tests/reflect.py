c366570775fa45fd1dbe1866c43508295a40d48a
Mats Gustafsson <hem@mats-gustafsson.se>
2017-10-15 08:40:19 +0200

    corrects some bugs when writing to log file

--- tests/reflect.py
+++ tests/reflect.py
@@ -9,53 +9,56 @@ from optparse import OptionParser
 
 PORT = 8085
 HTTP_LOG_FILE = 'tests/data/http_log_file.log'
+SEND_RESPONSE = False
+
 
 class RequestHandler(BaseHTTPRequestHandler):
-    
     def do_GET(self):
-        
+
         request_path = self.path
         with open(HTTP_LOG_FILE, 'a') as log_file:
             log_file.write("\n----- GET Request Start ----->\n")
-            log_file.write("Request path:", request_path)
-            log_file.write("Request headers:", self.headers)
+            log_file.write("Request path: {}\n".format(request_path))
+            log_file.write("Request headers: {}\n".format(self.headers))
             log_file.write("<----- Request End -----\n")
-        
-        self.send_response(200)
-        self.send_header("Set-Cookie", "foo=bar")
-        self.end_headers()
-        
+
+        if SEND_RESPONSE:
+            self.send_response(200)
+            self.send_header("Set-Cookie", "foo=bar")
+            self.end_headers()
+
     def do_POST(self):
-        
+
         request_path = self.path
         with open(HTTP_LOG_FILE, 'a') as log_file:
             log_file.write("\n----- POST Request Start ----->\n")
-            log_file.write("Request path:", request_path)
+            log_file.write("Request path: {}\n".format(request_path))
             request_headers = self.headers
             content_length = request_headers.get('Content-Length')
             length = int(content_length) if content_length else 0
-            log_file.write("Content Length:", length)
-            log_file.write("Request headers:", request_headers)
-            log_file.write("Request payload:", self.rfile.read(length))
+            log_file.write("Content Length: {}\n".format(length))
+            log_file.write("Request headers: {}\n".format(request_headers))
+            payload = self.rfile.read(length)
+            log_file.write("Request payload: {}\n".format(payload))
             log_file.write("<----- Request End -----\n")
-        
-        self.send_response(200)
-        self.end_headers()
-    
+
+        if SEND_RESPONSE:
+            self.send_response(200)
+            self.end_headers()
+
     do_PUT = do_POST
     do_DELETE = do_GET
-        
+
+
 def main():
     port = PORT
-    #print('Listening on localhost:%s' % port)
     server = HTTPServer(('', port), RequestHandler)
     server.serve_forever()
 
-        
+
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.usage = ("Creates an http-server that will echo out any GET or POST parameters\n"
-                    "Run:\n\n"
-                    "   reflect")
+    usage_text = "Creates http-server that will echo GET or POST parameters\n"
+    parser.usage = (usage_text)
     (options, args) = parser.parse_args()
     main()
