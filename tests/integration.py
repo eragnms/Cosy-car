645af9332e8d864059442a59b34ed845c27e8639
Mats Gustafsson <hem@mats-gustafsson.se>
2017-10-19 23:03:15 +0200

    adds extraction of info from log file

--- tests/integration.py
+++ tests/integration.py
@@ -4,13 +4,24 @@
 import os
 import time
 import subprocess
+import traceback
+import signal
+import re
+
+HTTP_LOG_FILE = 'tests/data/http_log_file.log'
 
 def main():
     test_cases = init_test_cases()
     for test_case in test_cases:
         process = setup()
-        test_case.run()
-        teardown(process)
+        try:
+            test_case.run()
+        except TestFailure as e:
+            teardown(process)
+            raise TestFailure(test_case.description) from e
+        except Exception:
+            teardown(process)
+            raise Exception(traceback.format_exc())
 
 def init_test_cases():
     test_cases = [TestForcedStart(),
@@ -22,7 +33,9 @@ def setup():
         os.remove(HTTP_LOG_FILE)
     except:
         pass
-    process = subprocess.Popen(['tests/reflect.py'],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen(['tests/reflect.py', '-p 8085'],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
     return process
 
 def teardown(process):
@@ -51,11 +64,8 @@ class TestForcedStart():
     def run(self):
         os.system('cosycar >/dev/null 2>&1')
         test_engine = TestEngine(self.heaters)
-        try:
-            test_engine.run(self.run_time)
-        except IntegrationError as e:
-            raise IntegrationError(self.description + ': ' + e.value)
-        
+        test_engine.run(self.run_time)
+                
 class TestSomethingElse():
     def __init__(self):
         self.description = 'TestSomethingElse'
@@ -75,7 +85,7 @@ class Heater():
         if is_actually_on != expected_to_be_on:
             error_text = 'Heater with id {} has status {}, expected'
             error_text += ' status {}, time is {}'
-            raise IntegrationError(error_text.format(self.zwave_id,
+            raise TestFailure(error_text.format(self.zwave_id,
                                                      is_actually_on,
                                                      expected_to_be_on,
                                                      now))
@@ -101,20 +111,58 @@ class TestEngine():
                 os.system('cosycar >/dev/null 2>&1')
                 heater_statuses = self._check_heater_statuses()
                 for heater in self.heaters:
-                    heater.check_status(now, heater_statuses[heater.zwave_id])
+                    try:
+                        heater.check_status(now,
+                                            heater_statuses[heater.zwave_id])
+                    except KeyError:
+                        # ToDo: here we should mark that the heater did not
+                        # have any status info and raise an error if the
+                        # device never gets any status set.
+                        pass
                 next_cosycar_run += self._cosycar_run_period
             now = time.time() - test_start_time
 
     def _check_heater_statuses(self):
-        statuses = {10: False, 11: False}
-        return statuses
+        try:
+            heater_statuses = {}
+            with open(HTTP_LOG_FILE, 'r') as log_file:
+                for line in log_file:
+                    device_id = self._extract_device_id(line)
+                    device_state = self._extract_device_state(line)
+                    if device_id and device_state:
+                        heater_statuses['device_id'] = device_state
+        except (FileNotFoundError, FileExistsError):
+            pass
+        return heater_statuses
+
+    def _extract_device_id(self, line):
+        device_id = self._extract_info('DeviceNum', line)
+        return device_id
+
+    def _extract_device_state(self, line):
+        target_value = self._extract_info('TargetValue', line)
+        if target_value:
+            return True
+        else:
+            return False
+
+    def _extract_info(self, text, line):
+        info_with_text = re.findall(text + '\d+', line)
+        info = None
+        if info_with_text:
+            info = re.findall('\d+', str(info_with_text[0]))
+            if info:
+                info = selftr(info[0])
+        return info
+
+
 
      # On = http://$ip_address:3480/data_request?id=action&output_format=xml&DeviceNum=$my_id&serviceId=urn:upnp-org:serviceId:SwitchPower1&action=SetTarget&newTargetValue=1
      # Off = http://$ip_address:3480/data_request?id=action&output_format=xml&DeviceNum=$my_id&serviceId=urn:upnp-org:serviceId:SwitchPower1&action=SetTarget&newTargetValue=0
 
-    
 
-class IntegrationError(Exception):
+    
+class TestFailure(Exception):
     def __init__(self, value):
         self.value = value
     def __str__(self):
