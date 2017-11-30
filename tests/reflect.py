d6915004962d51626285e881a4387469c2f395a2
Mats Gustafsson <hem@mats-gustafsson.se>
2017-11-30 22:51:56 +0100

    cleaning up

--- tests/reflect.py
+++ tests/reflect.py
@@ -1,46 +1,43 @@
-#!/usr/bin/env python
+#!usr/bin/env python
 
-# Orinally written by Nathan Hamiel (2010), see:
-# https://gist.github.com/1kastner/e083f9e813c0464e6a2ec8910553e632
+# from: https://www.junian.net/2014/07/simple-http-server-and-client-in-python.html
 
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 import argparse
 
 DEFAULT_PORT = 8080
 HTTP_LOG_FILE = '/tmp/cosycar_http_log_file.log'
-SEND_RESPONSE = True
 
-class RequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-
-        request_path = self.path
-        with open(HTTP_LOG_FILE, 'a') as log_file:
-            log_file.write('{}\n'.format(request_path))
-
-        if SEND_RESPONSE:
-            self.send_response(200, "hejsansvejsan")
-            print("Sent...")
-            #self.send_response(json_data)
-            #self.send_header("Set-Cookie", "foo=bar")
-            self.end_headers()
-            self.flush_headers()
-            
-
-    def do_POST(self):
-
-        request_path = self.path
-        with open(HTTP_LOG_FILE, 'a') as log_file:
-            log_file.write('{}\n'.format(request_path))
-
-        if SEND_RESPONSE:
-            self.send_response(200)
-            self.end_headers()
-
-    do_PUT = do_POST
-    do_DELETE = do_GET
-
-
-def main():
+#Create custom HTTPRequestHandler class
+class KodeFunHTTPRequestHandler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    rootdir = '../tests/data'
+    try:
+      with open(HTTP_LOG_FILE, 'a') as log_file:
+        log_file.write('{}\n'.format(self.path))
+      if self.path.endswith('sdata'):
+        f = open(rootdir + "/response_1.html")
+      elif self.path.endswith('json'):
+        f = open(rootdir + "/response_1.html")
+      elif self.path.endswith('status'):
+        f = open(rootdir + "/response_1.html")
+      elif self.path.endswith('Value=1'):
+        f = open(rootdir + "/response_3.html")
+      elif self.path.endswith('Value=0'):
+        f = open(rootdir + "/response_4.html")
+      else:
+        f = open(rootdir + "/response_4.html")
+      self.send_response(200)
+      self.send_header('Content-type','text/html')
+      self.end_headers()
+      self.wfile.write(f.read().encode())
+      f.close()
+      return
+    except IOError:
+      self.send_error(404, 'file not found')
+  
+def run():
     usage_text = "Creates http-server that will echo GET or POST parameters\n"
     parser = argparse.ArgumentParser(description=usage_text)
     parser.add_argument("-p",
@@ -52,9 +49,13 @@ def main():
         port = args.port
     else:
         port = DEFAULT_PORT
-    server = HTTPServer(('', port), RequestHandler)
-    server.serve_forever()
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, KodeFunHTTPRequestHandler)
+    try:
+      httpd.serve_forever()
+    except KeyboardInterrupt:
+      pass
 
-
-if __name__ == "__main__":
-    main()
+    
+if __name__ == '__main__':
+  run()
