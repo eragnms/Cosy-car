4005304f4c11dd4b052091ba8b31c50c6228039d
Mats Gustafsson <hem@mats-gustafsson.se>
2017-12-25 23:18:50 +0100

    first weather test passes

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -12,7 +12,7 @@ from cosycar.weather import CosyWeather
 CFG_FILE = 'tests/data/cosycar_template.cfg'
 
 
-class CarSectionTests(unittest.TestCase):
+class WeatherTests(unittest.TestCase):
     def setUp(self):
         logging.basicConfig(
             filename='tests/data/cosycar.log',
@@ -21,7 +21,7 @@ class CarSectionTests(unittest.TestCase):
         self._config = configparser.ConfigParser()
         self._config.read(CFG_FILE)
         self._weather_interval = 10
-        self._weather_file = Constants.weather_storage_file
+        self._weather_file = "/tmp/test_weather_file"
 
     def tearDown(self):
         pass
@@ -37,18 +37,19 @@ class CarSectionTests(unittest.TestCase):
                              read_mock,
                              options_mock):
         now = datetime.datetime.now()
-        timestamp = now - datetime.timedelta(minutes=self._weather_interval)
+        timestamp = now - datetime.timedelta(minutes=self._weather_interval - 1)
         timestamp = timestamp.strftime('%Y,%m,%d,%H,%M')
         get_mock.return_value = timestamp
         weather = CosyWeather("Country",
                               "City",
                               "mykey",
-                              "/tmp/test_weather_file",
+                              self._weather_file,
                               self._weather_interval)
         weather_data = weather.get_weather()
-        expected = [read_mock(self._weather_file),
-                    read_mock(self._weather_file)]
-        self.assertTrue(read_mock.call_args_list == expected)
+        self.assertTrue(read_mock.call_count == 2)
+
+    def test_fetch_from_wunder(self):
+        self.assertTrue(False)
 
 
 if __name__ == '__main__':
