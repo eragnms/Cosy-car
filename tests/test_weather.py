5ce1941ea031cc41a35c5b6f5947b99bfc0a89fd
Mats Gustafsson <hem@mats-gustafsson.se>
2018-01-03 07:03:11 +0100

    upd tests for wunder fail

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -7,7 +7,7 @@ import configparser
 import datetime
 
 from cosycar.constants import Constants
-from cosycar.weather import CosyWeather
+from cosycar.weather import CosyWeather, CosyWeatherError
 
 CFG_FILE = 'tests/data/cosycar_template.cfg'
 
@@ -66,7 +66,29 @@ class WeatherTests(unittest.TestCase):
         weather.get_weather()
         self.assertTrue(fetch_mock.call_count == 1)
 
-    def test_fetch_from_wunder_failure(self):
+        # https://stackoverflow.com/questions/40736292/python-mock-raise-exception/40742247
+        
+    @patch('cosycar.weather.CosyWeather._save_weather')
+    @patch('cosycar.weather.CosyWeather._check_weather_data')
+    @patch('cosycar.weather.CosyWeather._decode_deserialize')
+    @patch('configparser.ConfigParser.options')
+    @patch('configparser.ConfigParser.read')
+    @patch('cosycar.weather.CosyWeather._fetch_wunder_weather')
+    @patch('configparser.ConfigParser.get')
+    def test_fetch_from_wunder_fail(self, get_mock, fetch_mock, read_mock,
+                                    options_mock, decode_mock, check_mock,
+                                    save_mock):
+        now = datetime.datetime.now()
+        timestamp = now - datetime.timedelta(
+            minutes=self._weather_interval + 1)
+        timestamp = timestamp.strftime('%Y,%m,%d,%H,%M')
+        get_mock.return_value = timestamp
+        weather = CosyWeather("Country", "City", "mykey", self._weather_file,
+                              self._weather_interval)
+        weather_json = {'current_observation': {'temp_c': 10, 'wind_kph': 5}}
+        decode_mock.return_value = weather_json
+        fetch_mock.side_effect = CosyWeatherError('Error')
+        weather.get_weather()
         self.assertTrue(False)
 
 if __name__ == '__main__':
